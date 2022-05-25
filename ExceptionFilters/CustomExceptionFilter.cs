7d1a8d00d7469bd74247c0462ede553bd8d0be01
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 10:55:41 +0530

    first commit

---
+++ ExceptionFilters/CustomExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionFilters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        private readonly ILoggerManager _logger;
+        public CustomExceptionFilter(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogInformation(context.Exception.Message);
+        }
+    }
+}
